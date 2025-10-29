d162bd5ec9e687b40c3a61d09fbad39cc013de49
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2025-10-29 14:23:05 +0900

    ✨ google auth url, session테스트 완료

--- project/apps/users/views.py
+++ project/apps/users/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+import secrets
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def google_auth_url(request: Request):
+    # state 생성. 보안상 사용자를 재확인하기 위함.
+    state = secrets.token_urlsafe(16)
+
+    # 세션에 저장.
+    request.session['oauth_state'] = state
+
+    return Response({"state": request.session['oauth_state']})
+
