29564ad221b9b533afbd3aee5e5105ee870d2a78
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2025-11-01 16:52:37 +0900

    ✨ created GoogleCalendar model

--- project/apps/google_calendar/models.py
+++ project/apps/google_calendar/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import User
+
+class GoogleCalendar(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='google_calendars',
+        db_column='user_id'
+    )
+    # google_calendar의 실제 id 값.
+    google_calendar_str_id = models.CharField(max_length=255, null=False)
+
+    # is_activated 사용자가 활성화 했는지 여부.
+    is_activated = models.BooleanField(null=False, default=True)
+    
+    # is_primary 구글 캘린더가 기본 캘린더인지 여부.
+    is_primary = models.BooleanField(null=False, default=False)
+    
+    
