3078ff5a8f0d70e021a3535472e7d4f3a5ddaac2
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2025-11-02 23:16:42 +0900

    🐛 다른 사용자도 같은 캘린더를 가질 수 있으니, user와 google_calendar의 쌍을 unique로 재지정.

--- project/apps/google_calendar/models.py
+++ project/apps/google_calendar/models.py
@@ -11,7 +11,7 @@ class GoogleCalendar(models.Model):
         db_column='user_id'
     )
     # google_calendar의 실제 id 값.
-    google_calendar_str_id = models.CharField(max_length=255, null=False, unique=True)
+    google_calendar_str_id = models.CharField(max_length=255, null=False)
 
     # google_calendar의 summary 값.
     summary = models.CharField(max_length=255, null=False)
@@ -22,4 +22,7 @@ class GoogleCalendar(models.Model):
     # is_primary 구글 캘린더가 기본 캘린더인지 여부.
     is_primary = models.BooleanField(null=False, default=False)
     
+    # user와 google_calendar_str_id의 조합을 unique로 설정.
+    class Meta:
+        unique_together = ('user', 'google_calendar_str_id')
     
