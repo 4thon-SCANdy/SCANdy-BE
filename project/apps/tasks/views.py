567d03587eb9455a3f3c8eac7a3db469240e868a
osein607 <osein607@naver.com>
2025-11-12 00:28:26 +0900

    🐛 llm 로직 디버깅 로그 추가 (#38)

--- project/apps/tasks/views.py
+++ project/apps/tasks/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
+import traceback
 
 from apps.users.services import JWTAuthentication
 from apps.ocr.views import OcrView
@@ -23,7 +24,7 @@ class TaskLLMView(OcrView):
 
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
-    
+
     @extend_schema(
         request=TaskCreateSerializer,
         parameters=[TOKEN_HEADER],
@@ -31,102 +32,117 @@ class TaskLLMView(OcrView):
     def post(self, request, *args, **kwargs): 
 
         # 1) 이미지 업로드 ->  ocr 처리 수행
-        ocr_response = super().post(request, *args, **kwargs)
-        
-        ocr_results = ocr_response.data.get("results", [])
-        task_id = ocr_response.data.get("task_id")
-        if not ocr_results:
+        try:
+            ocr_response = super().post(request, *args, **kwargs)
+
+            if not hasattr(ocr_response, "data"):
+                return Response(
+                    {"error": "OCR 응답이 비정상입니다."},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
+            
+            ocr_results = ocr_response.data.get("results", [])
+            task_id = ocr_response.data.get("task_id")
+            if not ocr_results:
+                return Response(
+                    {"error": "OCR 결과가 없습니다."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            
+            print("ocr_results:", ocr_results)
+
+            
+            # 2) ocr 결과 전처리
+            all_texts = []
+            for item in ocr_results:
+                if "texts" in item and isinstance(item["texts"], list):
+                    all_texts.extend(item["texts"])
+
+            if not all_texts:
+                return Response(
+                    {"error": "OCR에서 텍스트를 찾을 수 없습니다."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            print("ocr_all texts:", all_texts)
+
+            # 3) refine_ocr로 일정 문장 추출
+            refined_ocr = refine_ocr(all_texts)
+
+            if "error" in refined_ocr:
+                return Response(refined_ocr, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            if not refined_ocr:
+                return Response({"error": "OCR에서 일정을 추출할 수 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # 3) 각 텍스트에 대해 일정 생성
+            user = request.user
+            print("refined_ocr 결과:", refined_ocr)
+
+            if isinstance(refined_ocr, list):
+                refined_ocr = [r for r in refined_ocr if r.strip()]
+            else:
+                refined_ocr = [refined_ocr]
+
+            results = [] # 여러 문장을 받도록
+            for t in refined_ocr:
+                # 스케줄 생성 -> OCR 파싱
+                created = create_schedule(t)
+                if isinstance(created, dict) and "error" in created:
+                    print("create_schedule 실패:", created)
+                    continue
+                print("일정 생성", created)
+                parsed = parse_response(created)
+
+                if not parsed:
+                    continue    
+                results.extend(parsed)
+
+            print("create_schedule 결과:", results)
+
+            # 4) 겹치는 일정 조회 + 추천 시간 반환
+            recommends = []
+            for r in results:
+                start = r.get("start_datetime")
+                end = r.get("end_datetime")
+
+                if not start or not end:
+                    print("recommend_time skip: datetime 누락", r)
+                    continue  # None 값이면 생략
+                recommend = recommend_time(user, start, end, request)
+                recommends.append(recommend)
+
+            # response list용으로 변환
+            parsed_ocr = refined_ocr
+            if isinstance(refined_ocr, list) and len(refined_ocr) == 1 and isinstance(refined_ocr[0], str):
+                try:
+                    parsed_ocr = json.loads(refined_ocr[0])
+                    print("Response용 OCR 파싱 성공:", parsed_ocr)
+                except json.JSONDecodeError:
+                    print("Response용 OCR 파싱 실패, 원본 유지")
+
+            # 5) task 모델 업데이트
+            if task_id:
+                try:
+                    task = Task.objects.get(id=task_id)
+                    task.ocr_result = "\n".join(all_texts)
+                    task.llm_result = json.dumps(results, ensure_ascii=False)
+                    task.save(update_fields=["ocr_result", "llm_result"])
+                except Task.DoesNotExist:
+                    print(f"Task {task_id}가 없어 저장에 실패했습니다.")
+
+
             return Response(
-                {"error": "OCR 결과가 없습니다."},
-                status=status.HTTP_400_BAD_REQUEST,
+                {"task_id": task_id, "ocr_result": parsed_ocr, "llm_result": results, "recommendation": recommends},
+                status=status.HTTP_200_OK,
             )
         
-        print("ocr_results:", ocr_results)
-
-        
-        # 2) ocr 결과 전처리
-        all_texts = []
-        for item in ocr_results:
-            if "texts" in item and isinstance(item["texts"], list):
-                all_texts.extend(item["texts"])
-
-        if not all_texts:
+        except Exception as e:
+            print("🔥 TaskLLM Internal Error:", e)
+            traceback.print_exc()
             return Response(
-                {"error": "OCR에서 텍스트를 찾을 수 없습니다."},
-                status=status.HTTP_400_BAD_REQUEST,
+                {"detail": str(e), "traceback": traceback.format_exc().splitlines()},
+                status=500
             )
-        print("ocr_all texts:", all_texts)
-
-        # 3) refine_ocr로 일정 문장 추출
-        refined_ocr = refine_ocr(all_texts)
-
-        if "error" in refined_ocr:
-            return Response(refined_ocr, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-        if not refined_ocr:
-            return Response({"error": "OCR에서 일정을 추출할 수 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # 3) 각 텍스트에 대해 일정 생성
-        user = request.user
-        print("refined_ocr 결과:", refined_ocr)
-
-        if isinstance(refined_ocr, list):
-            refined_ocr = [r for r in refined_ocr if r.strip()]
-        else:
-            refined_ocr = [refined_ocr]
-
-        results = [] # 여러 문장을 받도록
-        for t in refined_ocr:
-            # 스케줄 생성 -> OCR 파싱
-            created = create_schedule(t)
-            if isinstance(created, dict) and "error" in created:
-                print("create_schedule 실패:", created)
-                continue
-            print("일정 생성", created)
-            parsed = parse_response(created)
-
-            if not parsed:
-                continue    
-            results.extend(parsed)
-
-        print("create_schedule 결과:", results)
-
-        # 4) 겹치는 일정 조회 + 추천 시간 반환
-        recommends = []
-        for r in results:
-            start = r.get("start_datetime")
-            end = r.get("end_datetime")
-
-            if not start or not end:
-                print("recommend_time skip: datetime 누락", r)
-                continue  # None 값이면 생략
-            recommend = recommend_time(user, start, end, request)
-            recommends.append(recommend)
-
-        # response list용으로 변환
-        parsed_ocr = refined_ocr
-        if isinstance(refined_ocr, list) and len(refined_ocr) == 1 and isinstance(refined_ocr[0], str):
-            try:
-                parsed_ocr = json.loads(refined_ocr[0])
-                print("Response용 OCR 파싱 성공:", parsed_ocr)
-            except json.JSONDecodeError:
-                print("Response용 OCR 파싱 실패, 원본 유지")
-
-        # 5) task 모델 업데이트
-        if task_id:
-            try:
-                task = Task.objects.get(id=task_id)
-                task.ocr_result = "\n".join(all_texts)
-                task.llm_result = json.dumps(results, ensure_ascii=False)
-                task.save(update_fields=["ocr_result", "llm_result"])
-            except Task.DoesNotExist:
-                print(f"Task {task_id}가 없어 저장에 실패했습니다.")
-
-
-        return Response(
-            {"task_id": task_id, "ocr_result": parsed_ocr, "llm_result": results, "recommendation": recommends},
-            status=status.HTTP_200_OK,
-        )
 
 # /task/task_id → image 링크 , llm, ocr 분석결과
 class TaskViewSet(viewsets.ModelViewSet):
